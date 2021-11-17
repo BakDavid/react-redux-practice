c752c705a9bdcb2669a1a0e8aa764153ec5a57f1
Bak David <bak.david96@gmail.com>
2021-11-17 23:14:46 +0200

    Added reducers, actions, store, dispatch and functionalities to site

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {decrement, increment, log} from "./actions";
+
 function App() {
+  const counter = useSelector(state => state.counterReducer);
+  const isLogged = useSelector(state => state.loggedReducer);
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      Hello
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(log())}>Logged status change</button>
+
+      {isLogged ? <h3>Only logged in user can see this</h3> : ''}
     </div>
   );
 }
